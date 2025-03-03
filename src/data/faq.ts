6e74e87286c55675563df6016a95d38881a495f6
Trevor Loula <trevorloula@outlook.com>
2025-03-02 20:47:33 -0800

    Add FAQ entry about app subscription model

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -9,5 +9,9 @@ export const faqs: IFAQ[] = [
     {
         question: 'What if I need help using the app?',
         answer: `We provide extensive in-app guides to help you. If you need further assistance, feel free to email us at ${siteDetails.helpEmail}, and we'll respond as quickly as possible.`
+    },
+    {
+        question: 'Why isn\'t this app free?',
+        answer: `We believe in providing a high-quality experience. Your subscription helps us maintain and improve the app, ensuring you get the best service possible. We appreciate your support!`
     }
 ];
