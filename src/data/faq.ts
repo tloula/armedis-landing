da8f8d45993963173b794c42bcb0f84895dcd6d3
Trevor Loula <trevorloula@outlook.com>
2025-03-04 00:13:42 -0800

    FAQ: Internet

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -3,6 +3,10 @@ import { siteDetails } from "./siteDetails";
 
 export const faqs: IFAQ[] = [
     {
+        question: `Does ${siteDetails.siteName} require an internet connection (WiFi or Mobile Data)?`,
+        answer: `Yes, ${siteDetails.siteName} requires an internet connection to function. If you lose your connection, you will not be able to checkin. If you miss your checkin because of this, your contacts will still be notified.`,
+    },
+    {
         question: `Can I use ${siteDetails.siteName} on multiple devices?`,
         answer: 'Absolutely! Your account syncs seamlessly across all your devices. You\'ll receive a checkin notification on all devices your signed in to.',
     },
