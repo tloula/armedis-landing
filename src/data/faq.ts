4b9f16e43389f4addd99d75be2025b0dbbd97e37
Trevor Loula <trevorloula@outlook.com>
2025-03-08 16:52:08 -0800

    Add FAQs

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -11,11 +11,23 @@ export const faqs: IFAQ[] = [
         answer: 'Absolutely! Your account syncs seamlessly across all your devices. You\'ll receive a checkin notification on all devices your signed in to.',
     },
     {
-        question: 'What if I need help using the app?',
-        answer: `We provide extensive in-app guides to help you. If you need further assistance, feel free to email us at ${siteDetails.helpEmail}, and we'll respond as quickly as possible.`
+        question: `Can I pause or disable check-ins temporarily?`,
+        answer: `Yes! You can pause check-ins for vacations, travel, or other circumstances without deleting your schedule. Just go to the checkin you want to pause and select "Disable".`
+    },
+    {
+        question: `How do I cancel my subscription?`,
+        answer: `You can manage or cancel your subscription anytime through the app store where you subscribed (Google Play or Apple App Store). You will still have access to your account until the end of your billing cycle.`
+    },
+    {
+        question: `What happens when my subscription ends?`,
+        answer: `When your subscription ends, you will still have access to the app, but all your checkins will be disabled.`
     },
     {
         question: `Why isn't ${siteDetails.siteName} free?`,
         answer: `Maintaining ${siteDetails.siteName} comes with real costs — app store fees, cloud hosting, SMS services, and business expenses. Instead of selling your data to cover these costs, we charge a small fee to keep things running smoothly.\nThis approach allows us to provide better support, improve our service, and continue building features that matter to you. As a family-run project, we truly appreciate your support in helping us keep ${siteDetails.siteName} going.`
-    }
+    },
+    {
+        question: `What if I need help using ${siteDetails.siteName}?`,
+        answer: `We provide extensive in-app guides to help you. If you need further assistance, feel free to email us at ${siteDetails.helpEmail}, and we'll respond as quickly as possible.`
+    },
 ];
