cd9578c29a455d830f3ecfec1a8b01dc3fe97113
tloula <trevorloula@outlook.com>
2025-08-07 18:15:27 -0700

    Google Play URL!

--- src/data/cta.ts
+++ src/data/cta.ts
@@ -2,5 +2,5 @@ export const ctaDetails = {
     heading: 'CheckIn More Often With The People You Care About',
     subheading: 'Get started by downloading CheckIn More today!',
     appStoreUrl: '#',
-    googlePlayUrl: '#'
+    googlePlayUrl: 'https://play.google.com/store/apps/details?id=com.hydroxygenlabs.armedis'
 }
